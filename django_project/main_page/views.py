4c33b57c7d5334ddb83db4cd0c01e152ff419eba
williamstrong <willyyouarent@gmail.com>
2018-03-19 14:12:11 -0700

    main page works

--- django_project/main_page/views.py
+++ django_project/main_page/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Page
 # Create your views here.
 
 class Main(LoginRequiredMixin, View):
     def get(self, request):
-        return HttpResponse('response')
+        # num_names = Pages.objects.all().count()
+        # num_urls = Pages.objects.all().count()
+        pages = Page.objects.all()
+        return HttpResponse(render(request, 'main_page/index.html', {'pages': pages}))
