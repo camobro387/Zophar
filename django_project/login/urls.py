b3414652ef94ac1d7e3d1754d7758c224ff36c62
williamstrong <willyyouarent@gmail.com>
2018-03-19 13:25:34 -0700

    logging in

--- django_project/login/urls.py
+++ django_project/login/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 from . import views
-
+from django.contrib.auth.views import logout
 urlpatterns = [
     url(r'^$', views.Main.as_view()),
     url(r'^create/$', views.Create.as_view()),
+    url(r'^logout/$', logout, {'next_page': '/login'}),
 ]
--- django_project/login/views.py
+++ django_project/login/views.py
@@ -1,25 +1,22 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponseRedirect
 from django.http import HttpResponse, HttpResponseForbidden
 from django.views import View
 from django.contrib.auth.models import User
 
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 
 from .forms import SignUpForm, AuthenticationForm, UserCreationForm
 from .forms import LoginForm, SignUpForm, AuthenticationForm
 # Create your views here.
+
 
 class Main(View):
     def get(self, request):
         return HttpResponse(render(request, 'login/index.html', {'form' : AuthenticationForm}))
 
     def post(self, request):
-
-        form = AuthenticationForm(request.POST)
-
-        form = AuthenticationForm(request.POST)
+        form = AuthenticationForm(request, request.POST)
         if form.is_valid():
-            form.save()
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password')
             user = authenticate(username=username, password=raw_password)
@@ -35,7 +32,6 @@ class Main(View):
             return HttpResponse('bad')
 
 
-
 class Create(View):
     def get(self, request):
         return HttpResponse(render(request, 'login/create.html', {'form' : SignUpForm}))
@@ -48,6 +44,6 @@ class Create(View):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return redirect('/login')
+            return redirect('/main_page')
         else:
             return HttpResponse('error')
