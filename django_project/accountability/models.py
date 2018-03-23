34794d89b7f3a462b90ee483696492b54b2b45b8
William Strong <william.strong@me.com>
2018-03-23 12:19:22 -0700

    model

--- django_project/accountability/models.py
+++ django_project/accountability/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from ..login.models import User
 
 # Create your models here.
+
+
+class ChuchCheckIn(models.Model):
+    '''
+    Contains all of the church check in data.
+    '''
+    checkin = models.BooleanField()
+    date = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
--- django_project/accountability/urls.py
+++ django_project/accountability/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    path('', views.Main)
+    url('', views.Main.as_view())
 ]
