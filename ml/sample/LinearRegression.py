4d575a797df9f7e6bd7c9298455bac757c37285a
timothyryu <32182416+timothyryu@users.noreply.github.com>
2018-03-29 12:28:17 -0700

    Update LinearRegression.py

--- ml/sample/LinearRegression.py
+++ ml/sample/LinearRegression.py
@@ -2,12 +2,12 @@
 import numpy as np
 import pandas as pd
 
-#import training dataset
+#import dataset
 dataset = pd.read_csv("QR3.csv")
 X = dataset.iloc[:,1:].values
 y = dataset.iloc[:,0].values
 
-#encode data (yes or no must be converted to 0 and 1)
+#encode data
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder = LabelEncoder()
 X[:, 3] = labelencoder.fit_transform(X[:, 3])
@@ -18,22 +18,24 @@ X[:, 7] = labelencoder.fit_transform(X[:, 7])
 onehotencoder = OneHotEncoder(categorical_features = [3,4,5,6,7])
 X = onehotencoder.fit_transform(X).toarray()
 
-#Avoid dummy variables
+#Avoid dummy variable
 X = X[:,[2,4,7,9,10,11,12,13,14,15,16,17,18,19,20]]
 
-#optimal model; via backward elimination
-import statsmodels.formula.api as sm
-X = np.append(np.ones((146,1)).astype(int), values = X, axis = 1)
-X_opt = X[:, [0,1,2,3,4,5,6,7,8,9,10,11,12,13,15]]
+#optimal model; backward elimination
+X = np.append(np.ones((147,1)).astype(int), values = X, axis = 1)
+X_opt = X[:, [1,5,6,7,9,15]]
 
-##splitting data into training set and test set
+
+##splitting data
 from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X_opt, y, test_size = .2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X_opt, y, test_size = .3, random_state = 0)
 
-#Fit model according to training set
+#Fit model
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train,y_train)
 
-#predict using fitted model
-y_pred = regressor.predict(X_test)
+def maturityLevel(userData):
+    #predict
+    y_pred = regressor.predict(userData)
+    return y_pred
