e0e263169695e6d879510f54a6432cac7f45edb7
timothyryu <32182416+timothyryu@users.noreply.github.com>
2018-03-29 17:01:43 -0700

    Update LinearRegression.py

--- ml/sample/LinearRegression.py
+++ ml/sample/LinearRegression.py
@@ -19,12 +19,11 @@ onehotencoder = OneHotEncoder(categorical_features = [3,4,5,6,7])
 X = onehotencoder.fit_transform(X).toarray()
 
 #Avoid dummy variable
-X = X[:,[2,4,7,9,10,11,12,13,14,15,16,17,18,19,20]]
+X = X[:,[1,3,5,7,9,10,11,12,13,14,15,16,17,18,19,20]]
 
 #optimal model; backward elimination
 X = np.append(np.ones((147,1)).astype(int), values = X, axis = 1)
-X_opt = X[:, [1,5,6,7,9,15]]
-
+X_opt = X[:, [0,1,2,4,6,7,8,9,10]]
 
 ##splitting data
 from sklearn.cross_validation import train_test_split
@@ -35,7 +34,9 @@ from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train,y_train)
 
+
 def maturityLevel(userData):
-    #predict
-    y_pred = regressor.predict(userData)
-    return y_pred
+    #optimal model; backward elimination
+    userData = [1] + userData
+    y_pred = regressor.predict([userData])
+    return round(y_pred,0)
